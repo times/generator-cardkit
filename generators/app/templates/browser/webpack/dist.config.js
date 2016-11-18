bc9d9a963ef5c9ff1db88b890f4290dbe9ac9893
Chris Hutchinson <chris_hutchinson@me.com>
2016-11-18 18:04:51 +0000

    Fix webpack config for browser dist

--- generators/app/templates/browser/webpack/dist.config.js
+++ generators/app/templates/browser/webpack/dist.config.js
@@ -14,7 +14,7 @@ config.plugins.push(new webpack.optimize.UglifyJsPlugin({
   mangle: {
     except: ['CardKit']
   }
-});
+}));
 
 // Set process.env.NODE_ENV to production
 config.plugins.push(new webpack.DefinePlugin({
